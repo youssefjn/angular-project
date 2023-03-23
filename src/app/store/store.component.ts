81d57142e900dc9725244da31d457c6bca0c2129
youssefjn <youssefjneyah@gmail.com>
2023-03-23 10:56:55 +0100

    store implementation

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
   styleUrls: ['./store.component.css']
 })
-export class StoreComponent {
+export class StoreComponent implements OnInit {
+
+  constructor(private http: HttpClient) {}
+products: any = [];
+cart: any = [];
+  ngOnInit() {
+    this.http.get('http://localhost:3000/api/products').subscribe((data) => {
+      this.products = data;
+    })
+  }
+  orderP(product: any) {
+    this.cart.push(product);
+  }
+  getTotalPrice() {
+    return this.cart.reduce((total:any, product:any) => total + product.price, 0);
+  }
+  
+  checkout() {
+    window.location.href = 'http://localhost:4200/login';
+  }
 
 }
